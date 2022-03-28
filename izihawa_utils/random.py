15c4ce7bd2ebb217ffbb9a81752516a9aef1e832
Pasha Podolsky <ppodolsky@me.com>
2022-03-28 14:18:50 +0300

    - [utils] Refactoring
    GitOrigin-RevId: ca352f28493f701c4f30d118710dae064dd92e53

--- izihawa_utils/random.py
+++ izihawa_utils/random.py
@@ -4,3 +4,7 @@ import string
 
 def random_string(length):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def generate_request_id(length: int = 12):
+    return random_string(length)
